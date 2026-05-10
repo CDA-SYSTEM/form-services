43e9fc4d5308f0106824649029613531e1d8c28f
Ilesandres <ilesandres6@gmail.com>
2026-05-10 15:47:54 -0500

    :sparkles: feat(catalogs): add service-types endpoint

--- src/modules/catalogs/catalogs.controller.ts
+++ src/modules/catalogs/catalogs.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { VehicleType } from '../../shared/types/vehicle-type.enum';
+import { ServiceType } from '../../shared/types/service-type.enum';
 
 @ApiTags('catalogs')
 @Controller('catalogs')
@@ -9,5 +10,11 @@ export class CatalogsController {
   @ApiOperation({ summary: 'Obtener tipos de vehiculo' })
   getVehicleTypes(): string[] {
     return Object.values(VehicleType);
+  }
+
+  @Get('service-types')
+  @ApiOperation({ summary: 'Obtener tipos de servicio' })
+  getServiceTypes(): string[] {
+    return Object.values(ServiceType);
   }
 }
